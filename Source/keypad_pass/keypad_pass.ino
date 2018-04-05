7eff5d1b393fc012bc7b114e620662bbed1806a2
Metallhead01 <Metallhead01@gmail.com>
2018-04-05 11:52:28 +0300

    Changes from entering pass from keyboard to reading it frome base.

--- Source/keypad_pass/keypad_pass.ino
+++ Source/keypad_pass/keypad_pass.ino
@@ -1,83 +1,84 @@
-#include "Keypad.h"  //библиотека клавиатуры
-#include <EEPROMex.h>
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2); // Устанавливаем дисплей
 
-unsigned long pass_timer;
 char key;
 String str_pass = "";
-byte pass_lenght, j;
-unsigned long int_pass; // 10 знаков моксимум!!
-char keys[4][3] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'},
-  {'7', '8', '9'},
-  {'*', '0', '#'}
+int i, int_pass, my_array[2] = {1234, 4321};
+unsigned long pass_timer;
+
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {
+  {'1', '4', '7', '*'},
+  {'2', '5', '8', '0'},
+  {'3', '6', '9', '#'},
+  {'A', 'B', 'C', 'D'}
 };
-byte rowPins[] = {12, 11, 10, 9};     // Подключены строки (4 пина)
-byte colPins[] = {8, 7, 6};          // подключены столбцы (4 пина)
-Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, 4, 3 ); //иниициализировать клавиатуру
+byte rowPins[ROWS] = {38, 40, 42, 44};
+byte colPins[COLS] = {46, 48, 50, 52};
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void setup() {
   Serial.begin(9600);
-  int_pass = EEPROM.readLong(0);     // вспоминаем пароль из памяти
-  str_pass = String(int_pass, DEC);  // переводим в строчный тип
-  pass_lenght = str_pass.length();   // получиаем длину пароля
-
-  str_pass = "";                       // сброс пароля (пустая строка)
-  pass_timer = millis();               // сброс таймера ввода пароля
-  while (1) {                          // бесконечный цикл
-    key = keypad.getKey();             // обработка нажатия
-    if (key != NO_KEY) {               // если была нажата
-      pass_timer = millis();           // сбросить таймер
-      if (key == '*') {                // если нажата *
-        int_pass = str_pass.toInt();   // перевести в число
-        EEPROM.writeLong(0, int_pass); // записать в память
-        pass_lenght = str_pass.length();   // получиаем длину пароля
-        break;                         // выйти из цикла
-      }
-      else if (key == '#') {           // если нажата #
-        str_pass = "";                 // начать ввод сначала
-      }
-      else {                           // если * не нажата
-        str_pass += key;               // прибавить нажатую цифру к паролю
-      }
-    }
-    if (millis() - pass_timer > 10000) {     // если сработал таймер
-      str_pass = String(int_pass, DEC);      // сбросить ввод и выйти из цикла
-      break;
-    }
-  }
+  lcd.init();
+  lcd.backlight();// Включаем подсветку дисплея
 }
 
+boolean arrayIncludeElement(int my_array[], int int_pass) {
+  for (i = 0; i < 2; i++) {
+    if (my_array[i] == int_pass) {   //проверяем, что ввели
+      return true;
+    }
+  }
+  return false;
+}
 void pass_check() {
-  key = keypad.getKey();             // обработка нажатия
-  if (key == '*') {                  // если была нажата *
-    pass_timer = millis();           // сбросить таймер
-    j = 0;
-    while (1) {                      // бесконечный цикл ввода пароля
+  key = keypad.getKey();                 // обработка нажатия
+  if (key == '*') {                      // если была нажата *
+    lcd.clear();                         // отчищаем дисплей
+    pass_timer = millis();               // сбросить таймер
+    while (1) {                          // бесконечный цикл
       key = keypad.getKey();             // обработка нажатия
       if (key != NO_KEY) {               // если была нажата
         pass_timer = millis();           // сбросить таймер
-        if (key == str_pass[j]) {        // если новая введённая цифра совпала с цифрой пароля
-          j++;                           // прибавить счётчик
-        } else {                         // если нет
-          j = 0;                         // начать с начала
-        }
-        if (j == pass_lenght) {          // если были введены все правильные цифры пароля
-          Serial.println("Success");
+        if (key == '*') {
+          int_pass = str_pass.toInt();   // если нажата *
           break;                         // выйти из цикла
         }
-        if (key == '#') {                // если нажата #
-          break;                         // выйти из цикла
+        else if (key == '#') {           // если нажата #
+          str_pass = "";                 // начать ввод сначала
+        }
+        else {                           // если * не нажата
+          str_pass += key;               // прибавить нажатую цифру к паролю
+
         }
       }
-      if (millis() - pass_timer > 10000) {    // если сработал таймер
-        break;                                // выйти из цикла
+      if (millis() - pass_timer > 10000) {     // если сработал таймер
+        str_pass = String(int_pass, DEC);      // сбросить ввод и выйти из цикла
+        break;
       }
     }
+    Serial.println(int_pass);
+
+    if (arrayIncludeElement(my_array, int_pass) == true) { // кладем в функцию
+      lcd.setCursor(6, 0);                                 //массив и результат ввода
+      lcd.print("Open");
+      Serial.println("Open");
+    }
+    else {
+      lcd.setCursor(5, 0);
+      lcd.print("Error!");
+      Serial.println("Error!");
+      lcd.setCursor(6, 1);
+      lcd.print(int_pass);
+    }
+    int_pass = 0;                        // очищаем переменные перед следующим
+    str_pass = "";                       //вводом
   }
 }
-
 void loop() {
   pass_check();
-
 }
